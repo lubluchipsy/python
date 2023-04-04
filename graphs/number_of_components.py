d8e6d5c01d6395f40de40cf92c8af3a48dc2899c
Марина Толстикова <tolstikova.ms@phystech.edu>
2023-04-04 13:35:02 +0300

    commit

---
+++ graphs/number_of_components.py
@@ -0,0 +1,36 @@
+def dfs(u):
+    colors[u] = 1
+    free_vertices.remove(u)
+    for v in adj_list[u]:  #для соседей
+        if colors[v] == 0:
+            dfs(v)
+    colors[u] = 2
+    return
+
+n_edges = int(input())
+vertices = set()
+edges_list = []  #список ребер
+
+for i in range(n_edges):
+    u, v = map(int, input().split())
+    edges_list.append([u, v])
+    vertices.add(u)
+    vertices.add(v)
+
+n_vertices = max(vertices) + 1
+adj_list = [set() for _ in range(n_vertices)]  #список смежности
+
+for edge in edges_list:
+    adj_list[edge[0]].add(edge[1])
+    adj_list[edge[1]].add(edge[0])
+
+colors = [0]*n_vertices
+
+n_components = 0
+free_vertices = set(range(n_vertices))
+while free_vertices:
+    for u in free_vertices:
+        break
+    dfs(u)
+    n_components += 1
+print(n_components)
